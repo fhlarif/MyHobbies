3f79221c00eac585b1e65487bbcf3b65b8b9f0fd
fathularif <fathulcode@gmail.com>
2020-10-20 17:18:57 +0800

    add condition

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,11 +81,11 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Hello {{auth()->user()->name}}
+                    Hello {{auth()->user()->name ?? 'there!'}}
                 </div>
 
                 <div class="links">
-                    <p>{{auth()->user()->email}}</p>
+                    <p>{{auth()->user()->email ?? ''}}</p>
                     <a href="https://laravel.com/docs">Docs</a>
                     <a href="https://laracasts.com">Laracasts</a>
                     <a href="https://laravel-news.com">News</a>
