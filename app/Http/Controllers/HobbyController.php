27beede371f6010ae8362f9d62c3f8fd39835b90
fathularif <fathulcode@gmail.com>
2020-10-21 09:42:11 +0800

    add simple validation

--- app/Http/Controllers/HobbyController.php
+++ app/Http/Controllers/HobbyController.php
@@ -36,6 +36,10 @@ class HobbyController extends Controller
      */
     public function store(Request $request)
     {
+        $request->validate([
+            'name'=>'required|min:3',
+            'description'=>'required|min:5',
+        ]);
         $hobby=new Hobby([
             'name'=>$request->name,
             'description'=>$request->description,
